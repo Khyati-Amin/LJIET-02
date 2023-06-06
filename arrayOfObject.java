356101e659422d0583fa83d2096683c3b5dd502c
Khyati-Amin <133674163+Khyati-Amin@users.noreply.github.com>
2023-06-06 23:34:09 +0530

    Add files via upload

---
+++ arrayOfObject.java
@@ -0,0 +1,49 @@
+package array;
+
+import java.util.*;
+
+class student
+{
+	Scanner sc = new Scanner(System.in);
+	public int rollno;
+	public String studentName;
+	public int marks;
+	
+	public void setData()
+	{
+		System.out.println("enter rollno: ");
+		rollno = sc.nextInt();
+		System.out.println("enter name: ");
+		studentName = sc.next();
+		System.out.println("enter marks: ");
+		marks = sc.nextInt();
+	}
+	
+	public void getData()
+	{
+		System.out.println("rollno is "+rollno+", name is "+studentName+", marks are "+marks);
+	}
+	
+	
+}
+public class arrayOfObject {
+	
+
+	public static void main(String[] args) {
+				
+		student[] s = new student[5];
+		
+		for(int i=0;i<5;i++)
+		{
+			s[i]= new student();
+			s[i].setData();
+		}
+		
+		for(int i=0;i<5;i++)
+		{
+			s[i].getData();
+		}
+		
+	}
+
+}
