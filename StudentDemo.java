cd46bcb8ac45bd2dc39c22cf9f3d7c05a1834585
Khyati-Amin <133674163+Khyati-Amin@users.noreply.github.com>
2023-06-06 23:35:05 +0530

    Add files via upload

---
+++ StudentDemo.java
@@ -0,0 +1,47 @@
+package array;
+import java.util.*;
+
+class Demo
+{
+	Scanner sc = new Scanner(System.in);
+	int rollno;
+	String name;
+	int marks;
+	String ans;
+	
+	public void setData()
+	{
+		System.out.println("enter rollno: ");
+		rollno = sc.nextInt();
+		System.out.println("enter name: ");
+		name = sc.next();
+		System.out.println("enter marks: ");
+		marks = sc.nextInt();
+	}
+	public void getData()
+	{
+		System.out.println("rollno is "+rollno+", name is "+name+", marks are "+marks);
+		
+		
+	}
+}
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		
+		Demo[] d = new Demo[5];
+		
+		int i=0;
+		
+		do
+		{
+			d[i]= new Demo();
+			d[i].setData();
+			d[i].getData();
+			i++;
+		}
+		while(i<5);
+	}
+
+}
